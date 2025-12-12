9ec29b2c0faf95773fa1f9d464c926a5f7bbfab8
Puk06 <86549420+puk06@users.noreply.github.com>
2025-12-12 09:30:52 +0900

    修正

--- Pukorufu-Docs-Launcher/Pukorufu-Docs.js
+++ Pukorufu-Docs-Launcher/Pukorufu-Docs.js
@@ -6,7 +6,7 @@ const PORT = 3008;
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get(/^\/AttenuationTool\/.+/, (req, res) => {
+app.get(/.+/, (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
