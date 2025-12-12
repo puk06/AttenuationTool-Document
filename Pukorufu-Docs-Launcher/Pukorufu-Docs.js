e70671f8d5ac9ceb4c129fafa228ba2c8e21cbec
Puk06 <86549420+puk06@users.noreply.github.com>
2025-12-12 09:07:51 +0900

    パスの修正

--- Pukorufu-Docs-Launcher/Pukorufu-Docs.js
+++ Pukorufu-Docs-Launcher/Pukorufu-Docs.js
@@ -4,10 +4,10 @@ const path = require("node:path");
 const app = express();
 const PORT = 3008;
 
-app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
