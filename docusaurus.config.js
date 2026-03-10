aaf4e47e9ceaf6dd13b1b34f0b1387d18dd8766b
Puk06 <86549420+puk06@users.noreply.github.com>
2026-03-10 12:40:36 +0900

    修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'pukorufu', // Usually your GitHub org/user name.
-  projectName: 'attenuationtool-document', // Usually your repo name.
+  organizationName: 'puk06', // Usually your GitHub org/user name.
+  projectName: 'AttenuationTool-Document', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
