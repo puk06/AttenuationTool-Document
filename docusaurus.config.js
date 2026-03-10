0912539f9a0151e1ca78b1520fea3f523757878d
Puk06 <86549420+puk06@users.noreply.github.com>
2026-03-10 12:36:53 +0900

    github pagesの設定に変更

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.pukosrv.net/',
+  url: 'https://pukorufu.github.io',
+  baseUrl: '/attenuationtool-document/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
